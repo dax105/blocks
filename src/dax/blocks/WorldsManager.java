1b7e5a01f4a924a50ab4da8cd9f181224310c6c4
ondryaso <ondryaso@LeOndryasONTB>
2014-07-06 21:25:25 +0200

    Fix try

--- src/dax/blocks/WorldsManager.java
+++ src/dax/blocks/WorldsManager.java
@@ -9,52 +9,53 @@ import dax.blocks.world.WorldInfo;
 
 public class WorldsManager {
 	public static final String SAVES_DIR = "saves";
-	
+
 	public WorldsManager() {
-		
+
 	}
-	
+
 	public List<File> getWorldsDirs() {
 		ArrayList<File> worlds = new ArrayList<File>();
-		
+
 		File file = new File(SAVES_DIR);
 		String[] names = file.list();
-		
-		for(String name : names)
-		{
-			File d = new File(SAVES_DIR, name);
-		    if (d.isDirectory())
-		    {
-		        worlds.add(d);
-		    }
+
+		if (names != null) {
+			for (String name : names) {
+				File d = new File(SAVES_DIR, name);
+				if (d.isDirectory()) {
+					worlds.add(d);
+				}
+			}
 		}
-		
+
 		return worlds;
 	}
-	
+
 	public List<WorldInfo> getWorldsInfo() {
 		ArrayList<WorldInfo> worlds = new ArrayList<WorldInfo>();
-		
+
 		List<File> dirs = getWorldsDirs();
-		for(File d : dirs) {
+		for (File d : dirs) {
 			try {
-				worlds.add(WorldInfo.constructFromFile(new File(d, "world.txt")));
+				worlds.add(WorldInfo
+						.constructFromFile(new File(d, "world.txt")));
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return worlds;
 	}
-	
+
 	public WorldInfo getWorld(String name) {
 		List<WorldInfo> worlds = getWorldsInfo();
-		for(WorldInfo i : worlds) {
-			if(i.getWorldName().equalsIgnoreCase(name)) {
+		for (WorldInfo i : worlds) {
+			if (i.getWorldName().equalsIgnoreCase(name)) {
 				return i;
 			}
 		}
-		
+
 		return null;
 	}
 }
