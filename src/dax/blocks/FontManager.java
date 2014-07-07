30e6948c6a91c4bba42c07f203effd27a1cb7ede
dax105 <jerry1921mail@gmail.com>
2014-07-08 00:33:40 +0200

    Cleanup

--- src/dax/blocks/FontManager.java
+++ src/dax/blocks/FontManager.java
@@ -1,14 +1,10 @@
 package dax.blocks;
 
 import java.awt.Font;
-import java.awt.FontFormatException;
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.util.ResourceLoader;
-
-
 
 public class FontManager {
 
