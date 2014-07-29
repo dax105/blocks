5f3a3f9b786a4464ac7351bc7d7c2bdfbb3d90bc
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2014-07-29 17:40:16 +0200

    Increment version number

--- src/dax/blocks/Start.java
+++ src/dax/blocks/Start.java
@@ -1,10 +1,13 @@
 package dax.blocks;
 
 public class Start {
-	public static final String GAME_VERSION = "1.3 GoT beta #205";
+	public static final String GAME_VERSION = "1.3 GoT beta #212";
 	public static final String GAME_NAME = "Order of the Stone";
 	public static void main(String[] args) {
 		Game game = new Game();
+		
+		for(String s : args)
+			System.out.println("Argument " + s + " accepted");
 		
 		if(args.length > 2) {
 			game.doLogin(args[0], args[1], args[2]);
