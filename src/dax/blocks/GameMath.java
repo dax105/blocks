b34d5e577cc8bc00009bae776ea03784c4169020
Lukas Pohlreich <pohlreichlukas@gmail.com>
2014-08-05 15:03:32 +0200

    Format GameMath to follow max 80columns rule

--- src/dax/blocks/GameMath.java
+++ src/dax/blocks/GameMath.java
@@ -2,7 +2,8 @@ package dax.blocks;
 
 public class GameMath {
 
-	public static double biLerp(double x, double y, double q11, double q12, double q21, double q22, double x1, double x2, double y1, double y2) {
+	public static double biLerp(double x, double y, double q11, double q12, double q21, 
+			double q22, double x1, double x2, double y1, double y2) {
 		double r1 = lerp(x, x1, x2, q11, q21);
 		double r2 = lerp(x, x1, x2, q12, q22);
 		return lerp(y, y1, y2, r1, r2);
@@ -16,7 +17,9 @@ public class GameMath {
 		return x1 * (1.0 - p) + x2 * p;
 	}
 
-	public static double triLerp(double x, double y, double z, double q000, double q001, double q010, double q011, double q100, double q101, double q110, double q111, double x1, double x2, double y1, double y2, double z1, double z2) {
+	public static double triLerp(double x, double y, double z, double q000, double q001, 
+			double q010, double q011, double q100, double q101, double q110, double q111, 
+			double x1, double x2, double y1, double y2, double z1, double z2) {
 		double x00 = lerp(x, x1, x2, q000, q100);
 		double x10 = lerp(x, x1, x2, q010, q110);
 		double x01 = lerp(x, x1, x2, q001, q101);
