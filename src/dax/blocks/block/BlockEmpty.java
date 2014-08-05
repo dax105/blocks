85746503dde6efff7fd3bc8d050253cbb21e9e52
LeOndryasO <ondryaso@ondryaso.eu>
2014-08-05 13:36:29 +0200

    Fixed string formatting

--- src/dax/blocks/block/BlockEmpty.java
+++ src/dax/blocks/block/BlockEmpty.java
@@ -20,20 +20,20 @@ public class BlockEmpty extends Block {
 	@Override
 	public void onTick(int x, int y, int z, World world) {
 		if(!isAir)
-			Logger.getGlobal().warning(String.format("Something went wrong - tick on null block at %1-%2-%3 in world %4", x, y, z, world.name));
+			Logger.getGlobal().warning(String.format("Something went wrong - tick on null block at %s-%s-%s in world %s", x, y, z, world.name));
 	}
 
 	@Override
 	public void onRenderTick(float partialTickTime, int x, int y, int z,
 			World world) {
 		if(!isAir)
-			Logger.getGlobal().warning(String.format("Something went wrong - render tick on null block at %1-%2-%3 in world %4", x, y, z, world.name));
+			Logger.getGlobal().warning(String.format("Something went wrong - render tick on null block at %s-%s-%s in world %s", x, y, z, world.name));
 	}
 
 	@Override
 	public void onClicked(int button, int x, int y, int z, World world) {
 		if(!isAir)
-			Logger.getGlobal().warning(String.format("Something went wrong - clicked on null block at %1-%2-%3 in world %4", x, y, z, world.name));
+			Logger.getGlobal().warning(String.format("Something went wrong - clicked on null block at %s-%s-%s in world %s", x, y, z, world.name));
 	}
 
 }
