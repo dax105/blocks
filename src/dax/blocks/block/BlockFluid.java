2942b83b00db9e8cf4f6928862eb56c7738a8b57
Lukas Pohlreich <pohlreichlukas@gmail.com>
2014-08-06 11:12:45 +0200

    BlockFluid.java cleanup

--- src/dax/blocks/block/BlockFluid.java
+++ src/dax/blocks/block/BlockFluid.java
@@ -18,27 +18,27 @@ public class BlockFluid extends BlockBasic {
 	
 	@Override
 	public void onTick(int x, int y, int z, World world) {
-		if (world.getBlock(x, y-1, z) == Block.water.getId()) {
+		if(world.getBlock(x, y-1, z) == Block.water.getId()) {
 			return;
 		}
 		
 		Block b = Block.getBlock(world.getBlock(x, y - 1, z));
 		boolean isPlant = (b != null && b instanceof BlockPlant);
 		
-		if (world.getBlock(x, y-1, z) == 0 || isPlant) {
+		if(world.getBlock(x, y-1, z) == 0 || isPlant) {
 			world.setBlock(x, y-1, z, Block.water.getId(), true, true);
 			return;
 		}
-		if (world.getBlock(x+1, y, z) == 0 || isPlant) {
+		if(world.getBlock(x+1, y, z) == 0 || isPlant) {
 			world.setBlock(x+1, y, z, Block.water.getId(), true, true);
 		}
-		if (world.getBlock(x-1, y, z) == 0 || isPlant) {
+		if(world.getBlock(x-1, y, z) == 0 || isPlant) {
 			world.setBlock(x-1, y, z, Block.water.getId(), true, true);
 		}
-		if (world.getBlock(x, y, z+1) == 0 || isPlant) {
+		if(world.getBlock(x, y, z+1) == 0 || isPlant) {
 			world.setBlock(x, y, z+1, Block.water.getId(), true, true);
 		}
-		if (world.getBlock(x, y, z-1) == 0 || isPlant) {
+		if(world.getBlock(x, y, z-1) == 0 || isPlant) {
 			world.setBlock(x, y, z-1, Block.water.getId(), true, true);
 		}
 	}
