0483c01a9034182fbbcea7f7b177bb599b7c67cb
Lukas Pohlreich <pohlreichlukas@gmail.com>
2014-08-06 13:47:49 +0200

    Item.java cleanup

--- src/dax/blocks/item/Item.java
+++ src/dax/blocks/item/Item.java
@@ -1,17 +1,19 @@
 package dax.blocks.item;
 
 public abstract class Item {
+	
 	//Special positions: 0 - null item; 1 - null block; 2 - air
 	private static Item[] allItems = new Item[512];
 	private static int itemsCount;
-	
 	public static Item nullItem = new ItemEmpty();
 	
+	private int id;
+	
 	public static void registerItem(Item i, int id) throws RegisterException {
-		if(id < allItems.length) {
-			if(allItems[id] == null) {
-				allItems[id] = i;
-				itemsCount++;
+		if(id < Item.allItems.length) {
+			if(Item.allItems[id] == null) {
+				Item.allItems[id] = i;
+				Item.itemsCount++;
 				return;
 			}
 		}
@@ -20,24 +22,21 @@ public abstract class Item {
 	}
 	
 	public static int getItemsCount() {
-		return itemsCount;
+		return Item.itemsCount;
 	}
 	
 	public static Item getItem(int id) {
-		if(id < allItems.length)
-			return allItems[id];
+		if(id < Item.allItems.length)
+			return Item.allItems[id];
 		else
 			return null;
 	}
-	
-	
-	private int id;
 	
 	public Item(int itemID) {
 		this.id = itemID;
 		
 		try {
-			Item.registerItem(this, id);
+			Item.registerItem(this, this.id);
 		} catch (RegisterException e) {
 			e.printStackTrace();
 		}
@@ -45,7 +44,6 @@ public abstract class Item {
 	
 	public int getId() {
 		return this.id;
-
 	}
 	
 	public abstract void onTick(int itemIdentifier);
