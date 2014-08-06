a724cd5f3405808e0ed649069c69ba3fb6ccf322
Lukas Pohlreich <pohlreichlukas@gmail.com>
2014-08-06 11:11:51 +0200

    Block.java cleanup

--- src/dax/blocks/block/Block.java
+++ src/dax/blocks/block/Block.java
@@ -41,23 +41,34 @@ public abstract class Block extends Item {
 	
 	public static final Block air = new BlockEmpty(true);
 	public static final Block nullBlock = new BlockEmpty(false);
-	public static final Block grass = new BlockBasic(3).setTopTexture(4).setSideTexture(5).setBottomTexture(3).setStepSound(SoundManager.footstep_grass).setFallSound("fall_soft");
-	public static final Block dirt = new BlockBasic(4).setAllTextures(3).setStepSound(SoundManager.footstep_dirt).setFallSound("fall_soft");
+	public static final Block grass = new BlockBasic(3).setTopTexture(4).setSideTexture(5)
+		.setBottomTexture(3).setStepSound(SoundManager.footstep_grass).setFallSound("fall_soft");
+	public static final Block dirt = new BlockBasic(4).setAllTextures(3)
+		.setStepSound(SoundManager.footstep_dirt).setFallSound("fall_soft");
 	public static final Block stone = new BlockStone();
-	public static final Block wood = new BlockBasic(6).setAllTextures(2).setStepSound(SoundManager.footstep_wood).setFallSound("fall_hard");
-	public static final Block stoneMossy = new BlockBasic(7).setAllTextures(1).setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard");
-	public static final Block bricks = new BlockBasic(8).setAllTextures(8).setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard");
+	public static final Block wood = new BlockBasic(6).setAllTextures(2)
+		.setStepSound(SoundManager.footstep_wood).setFallSound("fall_hard");
+	public static final Block stoneMossy = new BlockBasic(7).setAllTextures(1)
+		.setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard");
+	public static final Block bricks = new BlockBasic(8).setAllTextures(8)
+		.setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard");
 	public static final Block sand = new BlockSand();
-	public static final Block log = new BlockBasic(10).setAllTextures(11).setSideTexture(7).setStepSound(SoundManager.footstep_wood).setFallSound("fall_hard");
-	public static final Block glass = new BlockBasic(11).setAllTextures(9).setOpaque(false).setCullSame(true).setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard").setRenderPass(RenderPass.TRANSPARENT);
+	public static final Block log = new BlockBasic(10).setAllTextures(11).setSideTexture(7)
+		.setStepSound(SoundManager.footstep_wood).setFallSound("fall_hard");
+	public static final Block glass = new BlockBasic(11).setAllTextures(9).setOpaque(false)
+		.setCullSame(true).setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard")
+		.setRenderPass(RenderPass.TRANSPARENT);
 	public static final Block leaves = new BlockBasic(12)
 		.setAllTextures(Settings.getInstance().transparentLeaves.getValue() ? 10 : 19)
 		.setOpaque(!Settings.getInstance().transparentLeaves.getValue())
 		.setStepSound(SoundManager.footstep_grass).setFallSound("fall_soft")
 		.setRenderPass(Settings.getInstance().transparentLeaves.getValue() ? RenderPass.TRANSPARENT : RenderPass.OPAQUE);
-	public static final Block bedrock = new BlockBasic(13).setAllTextures(12).setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard");
-	public static final Block water = new BlockFluid(14).setAllTextures(13).setCullSame(true).setOccluder(false).setOpaque(false).setDensity(1.175f);
-	public static final Block ice = new BlockBasic(15).setAllTextures(14).setOpaque(false).setCullSame(true).setRenderPass(RenderPass.TRANSLUCENT);
+	public static final Block bedrock = new BlockBasic(13).setAllTextures(12)
+		.setStepSound(SoundManager.footstep_stone).setFallSound("fall_hard");
+	public static final Block water = new BlockFluid(14).setAllTextures(13)
+		.setCullSame(true).setOccluder(false).setOpaque(false).setDensity(1.175f);
+	public static final Block ice = new BlockBasic(15).setAllTextures(14)
+		.setOpaque(false).setCullSame(true).setRenderPass(RenderPass.TRANSLUCENT);
 	public static final Block tallgrass = new BlockPlant(16).setAllTextures(15);
 	public static final Block flower_1 = new BlockPlant(17).setAllTextures(16);
 	public static final Block flower_2 = new BlockPlant(18).setAllTextures(17);
@@ -75,7 +86,7 @@ public abstract class Block extends Item {
 	
 	public Block(int id) {
 		super(id);
-		blocksCount++;
+		Block.blocksCount++;
 	}
 		
 	public Block requiresTick() {
@@ -198,11 +209,11 @@ public abstract class Block extends Item {
 	}
 
 	public float getDensity() {
-		return density;
+		return this.density;
 	}
 
 	public int getRenderPass() {
-		return renderPass;
+		return this.renderPass;
 	}
 	
 	public AABB getAABB() {
