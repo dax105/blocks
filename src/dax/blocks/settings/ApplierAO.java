15770bbc2965444be5834af4e7a3c9529a47d855
Lukas Pohlreich <pohlreichlukas@gmail.com>
2014-08-06 20:49:52 +0200

    ApplierAO.java cleanup

--- src/dax/blocks/settings/ApplierAO.java
+++ src/dax/blocks/settings/ApplierAO.java
@@ -9,9 +9,14 @@ public class ApplierAO extends Applier {
 	@Override
 	public boolean apply(Object val) {
 
-		if (Game.getInstance().ingame) {
+		if(Game.getInstance().ingame) {
 			Game.getInstance().world.saveAllChunks();
-			Game.getInstance().world = new World(false, Game.getInstance(), true, Game.getInstance().world.name);
+			Game.getInstance().world = new World(
+					false, 
+					Game.getInstance(), 
+					true, 
+					Game.getInstance().world.name
+			);
 		}
 		
 		Game.getInstance().displayLoadingScreen();
