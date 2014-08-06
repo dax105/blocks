4ecd006130d26f721f333b0107afb4028d70b702
Lukas Pohlreich <pohlreichlukas@gmail.com>
2014-08-06 11:16:36 +0200

    Console.java cleanup

--- src/dax/blocks/console/Console.java
+++ src/dax/blocks/console/Console.java
@@ -11,34 +11,31 @@ import dax.blocks.Game;
 
 public class Console {
 
-	private static Console _instance;
+	public static final int MAX_LINES = 100;
+	public static final int MAX_MEMORY = 100;
+	private static Console instance;
+
+	public List<String> lines = new ArrayList<String>();
+	public List<String> memory = new ArrayList<String>();
+
+	public CommandManager manager = new CommandManager();
+	public String currentCommand = "";
+	public int memPos = 0;
+	public boolean memUsed = false;
+	public int displayOffset = 0;
+	private Date now = new Date();
+	private String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-!,=%()[]{}<>#&@* ";
+
 	public static Console getInstance() {
-		if(_instance == null)
-			_instance = new Console();
+		if(Console.instance == null)
+			Console.instance = new Console();
 		
-		return _instance;
+		return Console.instance;
 	}
 	
 	public static void println(String ln) {
 		getInstance().out(ln);
 	}
-	
-	public static final int MAX_LINES = 100;
-	public static final int MAX_MEMORY = 100;
-	
-	public List<String> lines = new ArrayList<String>();
-	public List<String> memory = new ArrayList<String>();
-	
-	public CommandManager manager = new CommandManager();
-	public String currentCommand = "";
-	public int memPos = 0;
-	public boolean memUsed = false;
-	
-	public int displayOffset = 0;
-	
-	private Date now = new Date();
-	
-	private String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-!,=%()[]{}<>#&@* ";
 	
 	private Console() {
 		
@@ -49,116 +46,115 @@ public class Console {
 	}
 	
 	public void out(String text) {
-		now = new Date();
-		System.out.println("[INFO] " + now + ": " + text);
-		lines.add(text);
-		if (lines.size() > MAX_LINES) {
-			lines.remove(0);
+		this.now = new Date();
+		System.out.println("[INFO] " + this.now + ": " + text);
+		this.lines.add(text);
+		if(this.lines.size() > Console.MAX_LINES) {
+			this.lines.remove(0);
 		}
 	}
 	
 	public void addToMemory(String text) {
-		memory.add(text);
-		if (memory.size() > MAX_MEMORY) {
-			memory.remove(0);
+		this.memory.add(text);
+		if(this.memory.size() > Console.MAX_MEMORY) {
+			this.memory.remove(0);
 		}
 	}
 	
 	public void memUp() {
-		if (memUsed) {
-			if (memPos - 1 >= 0) {
-				memPos--;
-				setCommandFromMem();
+		if(this.memUsed) {
+			if(this.memPos - 1 >= 0) {
+				this.memPos--;
+				this.setCommandFromMem();
 			}
-		} else if (memory.size() > 0) {
-			memUsed = true;
-			memPos = memory.size()-1;
-			setCommandFromMem();
+		} else if(this.memory.size() > 0) {
+			this.memUsed = true;
+			this.memPos = this.memory.size()-1;
+			this.setCommandFromMem();
 		}
 	}
 	
 	public void memDown() {
-		if (memUsed) {
-			if (memPos + 1 < memory.size()) {
-				memPos++;
-				setCommandFromMem();
+		if(this.memUsed) {
+			if(this.memPos + 1 < this.memory.size()) {
+				this.memPos++;
+				this.setCommandFromMem();
 			} else {
-				memUsed = false;
-				clearInput();
+				this.memUsed = false;
+				this.clearInput();
 			}
 		}
 	}
 	
 	public void setCommandFromMem() {
-		currentCommand = memory.get(memPos);
+		this.currentCommand = this.memory.get(this.memPos);
 	}
 	
 	public void offsetDown() {
-		displayOffset -= 1;
+		this.displayOffset -= 1;
 	}
 	
 	public void offsetUp() {
-		displayOffset += 1;
+		this.displayOffset += 1;
 	}
 	
 	public float getTranslation() {
-		return -Game.getInstance().font.getLineHeight() * displayOffset;
+		return -Game.getInstance().font.getLineHeight() * this.displayOffset;
 	}
 	
 	public void charTyped(char c, int key) {
-		if (characters.contains("" + c)) {
-			currentCommand += c;
-			displayOffset = 0;
+		if(this.characters.contains("" + c)) {
+			this.currentCommand += c;
+			this.displayOffset = 0;
 		}
 		
-		if (Keyboard.KEY_UP == key) {
+		if(Keyboard.KEY_UP == key) {
 			
-			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-				offsetUp();
+			if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+				this.offsetUp();
 			} else {
-				memUp();
+				this.memUp();
 			}
 
 		}
 		
-		if (Keyboard.KEY_DOWN == key) {
-			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
-				offsetDown();
+		if(Keyboard.KEY_DOWN == key) {
+			if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+				this.offsetDown();
 			} else {
-				memDown();
+				this.memDown();
 			}
 		}
 		
-		if (Keyboard.KEY_BACK == key && currentCommand.length() > 0) {
-			currentCommand = currentCommand.substring(0, currentCommand.length() - 1);
+		if(Keyboard.KEY_BACK == key && this.currentCommand.length() > 0) {
+			this.currentCommand = this.currentCommand.substring(0, this.currentCommand.length() - 1);
 		}
 		
-		if (Keyboard.KEY_RETURN == key && currentCommand != "") {
-			executeCommand();
+		if(Keyboard.KEY_RETURN == key && this.currentCommand != "") {
+			this.executeCommand();
 		}
 	}
 	
 	public void executeCommand() {
-		memUsed = false;
-		addToMemory(currentCommand);
-		out("> " + currentCommand);
-		String[] words = currentCommand.split(" ");
+		this.memUsed = false;
+		this.addToMemory(this.currentCommand);
+		this.out("> " + this.currentCommand);
+		String[] words = this.currentCommand.split(" ");
 		String[] args = Arrays.copyOfRange(words, 1, words.length);
-		clearInput();
+		this.clearInput();
 		
-		if (words.length > 0) {
+		if(words.length > 0) {
 			Command command;
-			command = manager.getCommand(words[0].toLowerCase());
-			if (command != null) {
+			command = this.manager.getCommand(words[0].toLowerCase());
+			if(command != null) {
 				command.execute(args);
 			} else {
-				if (!words[0].equalsIgnoreCase("")) {
-					out("Unknown command \"" + words[0] + "\"");
+				if(!words[0].equalsIgnoreCase("")) {
+					this.out("Unknown command \"" + words[0] + "\"");
 				} else {
-					out("");
+					this.out("");
 				}
 			}
 		}
 	}
-
 }
