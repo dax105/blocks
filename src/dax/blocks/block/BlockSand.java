42372775d8eaedf5dfd860cb6b8eac4c28cd3926
dax105 <jerry1921mail@gmail.com>
2014-08-08 01:11:32 +0200

    Sand falling now works

--- src/dax/blocks/block/BlockSand.java
+++ src/dax/blocks/block/BlockSand.java
@@ -2,22 +2,30 @@ package dax.blocks.block;
 
 import dax.blocks.sound.SoundManager;
 import dax.blocks.world.IDRegister;
+import dax.blocks.world.UpdateType;
 import dax.blocks.world.World;
 
 public class BlockSand extends BlockBasic {
 
 	public BlockSand(IDRegister r) {
 		super("oots/blockSand", r);
-		this.setAllTextures(6).setFootStepSound(SoundManager.footstep_dirt).setFallSound("fall_soft");
+		this.setAllTextures(6).setFootStepSound(SoundManager.footstep_dirt)
+				.setFallSound("fall_soft");
 	}
 
 	@Override
-	public void onTick(int x, int y, int z, World world) {
-		super.onTick(x, y, z, world);
-		int below = world.getBlock(x, y - 1, z);
-		if(below == 0 || !world.getBlockObject(below).isCollidable()) {
-			world.setBlock(x, y - 1, z, 7, true, true);
-			world.setBlock(x, y, z, 0, true, true);
+	public void onNeighbourUpdate(int x, int y, int z, World world) {
+		world.scheduleUpdate(x, y, z, 0, UpdateType.SAND_FALL);
+	}
+
+	@Override
+	public void onUpdate(int x, int y, int z, int type, World world) {
+		if(type == UpdateType.SAND_FALL) {
+			int below = world.getBlock(x, y - 1, z);
+			if(below == 0 || !world.getBlockObject(below).isCollidable()) {
+				world.setBlock(x, y - 1, z, 7, true, true);
+				world.setBlock(x, y, z, 0, true, true);
+			}
 		}
 	}
 
